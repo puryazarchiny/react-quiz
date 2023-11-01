1b6ba7d385dd3ed40a31ddd42c76e2be18a0229f
Purya Zarchiny <puryazarchiny@gmail.com>
2023-11-01 07:27:32 +0330

    Add CORS header

--- server.cjs
+++ server.cjs
@@ -3,6 +3,16 @@ const jsonServer = require("json-server");
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://react-quiz-web.pages.dev",
+  );
+  next();
+});
 
 server.use(router);
 server.listen(3000);
